6d60facda1f69d10090dcc8ebc19cd94aed1e7b2
otinabrayo <110743391+otinabrayo@users.noreply.github.com>
2022-09-22 10:10:23 +0300

    Add files via upload

---
+++ style.js
@@ -0,0 +1,16 @@
+let card = document.getElementById("one")
+
+function effect(){
+    onmouseenter(style.border='1px solid red');
+}
+
+
+// onmouseenter = "style.transform='scale(1.1)'"
+// <button type="button"
+// onclick="document.getElementById('id1').style.color = 'red'">
+// Click Me!</button> 
+// function showMenu(){
+//     navLink.style.right = "0px";
+// }
+// function hideMenu(){
+//     navLink.style.right = "-200px";
